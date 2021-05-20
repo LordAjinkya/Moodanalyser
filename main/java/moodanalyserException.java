f1c1c8212e3a9e3624fc76aafff8e43e9fe94c03
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-20 22:45:50 +0530

    Added:welcome msg

---
+++ main/java/moodanalyserException.java
@@ -0,0 +1,3 @@
+public class moodanalyserException extends Throwable {
+    
+}
---
+++ test/java/com/moodanalyser/moodanalysertest.java
@@ -0,0 +1,9 @@
+package com.moodanalyser;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class moodanalysertest<moodanalyser> {
+
+}
